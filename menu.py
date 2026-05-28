0c834913019ab6dbb3d8f5e188066c947adfa604
Kaue Tsuyoshi <kauetsuyoshi1@gmail.com>
2026-05-28 09:29:20 -0300

    Deletando menu.py

--- menu.py
+++
@@ -1,9 +0,0 @@
-def exibir_menu():
-    print("\n--- MENU EDS: CONTROLE DE POEIRA ---")
-    print("1. Configurar Parâmetros (Voltagem/Fases)")
-    print("2. Simular Campo Elétrico (Protótipo A)")
-    print("3. Calcular Força de Repulsão (Protótipo B)")
-    print("4. Simulação de Partículas (Protótipo C)")
-    print("5. Relatório de Eficiência (Protótipo D)")
-    print("6. Sobre o Projeto")
-    print("0. Sair")
